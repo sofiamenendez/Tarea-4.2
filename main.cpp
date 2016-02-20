28b9578f29c86472c0289702f7d306ef97698603
sofiamenendez <samenendez97@unitec.edu>
2016-02-20 16:39:30 -0600

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//encontrar los multiplos de 3 entre 100 -1000
+using namespace std;
+
+int main()
+{
+    int num,i;
+    int conta;
+
+    for(i=1000;i>=100;i--)
+    cout<<i<<"\n";
+
+      }while (!((num>=100) && (num<=1000)));
+
+        if (num%3==3)
+        {
+            conta++;
+
+        }
+
+
+    return 0;
+}
